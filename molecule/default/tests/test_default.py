d0909ea4ce31e13cba72882f94320c50b839c4aa
Joel Castillo <jocastillo@records.nyc.gov>
2019-08-28 09:24:50 -0400

    Remove proxy configuration from playbook

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,32 +7,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_proxy_profile_script_exists(host):
-    f = host.file("/etc/profile.d/proxy.sh")
-
-    assert f.exists
-    assert f.user == "root"
-    assert f.group == "root"
-
-
-def test_http_proxy_set(host):
-    cmd = host.run("env | grep http_proxy")
-
-    assert cmd.stdout.strip() == "http_proxy=http://bcpxy.nycnet:8080"
-
-
-def test_https_proxy_set(host):
-    cmd = host.run("env | grep https_proxy")
-
-    assert cmd.stdout.strip() == "https_proxy=http://bcpxy.nycnet:8080"
-
-
-def test_no_proxy_set(host):
-    cmd = host.run("env | grep no_proxy")
-
-    assert cmd.stdout.strip() == "no_proxy=localhost,127.0.0.1,::1"
-
-
 def test_rhsm_subscription_current(host):
     cmd = host.run("sudo subscription-manager status")
 
